ae14d18bbbca9b93d007cfc2ad9b19e99ab5a850
Erik Nikulski <bitbucket@eriknikulski.me>
2020-04-30 19:03:54 +0200

    Remove navigate prop error

--- frontend/src/components/NavBar/NavTitle/index.js
+++ frontend/src/components/NavBar/NavTitle/index.js
@@ -3,12 +3,17 @@ import React from 'react';
 import { Box, Typography, Link } from "@material-ui/core";
 import { Link as RouterLink } from "react-router-dom";
 
+const CustomLink = props => {
+  const { navigate, ...rest } = props;
+  return (<Link {...rest} />);
+}
+
 const NavTitle = ({ heading }) => {
   return (
     <Box pr={2} key={heading.name}>
       <RouterLink
         to={`/${heading.route}`}
-        component={Link}
+        component={CustomLink}
         color="inherit"
       >
         <Typography variant="h5">
