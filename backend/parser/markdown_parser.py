9838121af340bdeed5ced7a36b58fed058af895b
Erik Nikulski <bitbucket@eriknikulski.me>
2020-07-08 13:03:37 +0200

    Strip markdown

--- backend/parser/markdown_parser.py
+++ backend/parser/markdown_parser.py
@@ -1,3 +1,6 @@
+from markdown import Markdown
+from io import StringIO
+
 from parser.tex_parser import search as eq_search
 
 MATH_BLOCK_DELIMITERS = [
@@ -28,5 +31,31 @@ def search(file):
                                inline_delimiter=MATH_INLINE_DELIMITERS, file_type='md')
     text, code = eq_search(text, block_delimiter=CODE_DELIMITER,
                            inline_delimiter=[], file_type='md')
+    text = ' '.join(line for line in text if line != '')\
 
-    return ' '.join(text), code, equation
+    text = unmark(text)
+    print(text)
+
+    return text, code, equation
+
+
+def unmark_element(element, stream=None):
+    if stream is None:
+        stream = StringIO()
+    if element.text:
+        stream.write(element.text)
+    for sub in element:
+        unmark_element(sub, stream)
+    if element.tail:
+        stream.write(element.tail)
+    return stream.getvalue()
+
+
+# patching Markdown
+Markdown.output_formats["plain"] = unmark_element
+__md = Markdown(output_format="plain")
+__md.stripTopLevelTags = False
+
+
+def unmark(text):
+    return __md.convert(text)
