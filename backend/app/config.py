a1376f06208cc3ad51c1e08dac5144c01d6096c1
Guilherme Calabria Lopes <guicalabria@gmail.com>
2021-06-29 18:36:10 +0200

    move config file to new location inside /app

--- backend/app/config.py
+++ backend/app/config.py
@@ -9,5 +9,5 @@ class Config(object):
 
     # Flask-SQLAlchemy configuration parameters
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir, '../app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
