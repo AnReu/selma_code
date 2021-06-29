202941d84039eaea81524fb011b264b576fed487
Guilherme Calabria Lopes <guicalabria@gmail.com>
2021-06-29 18:31:54 +0200

    fix import

--- backend/models/VectorModel/predictor.py
+++ backend/models/VectorModel/predictor.py
@@ -1,23 +1,23 @@
-import backend.models.VectorModel.model
+from backend.models.VectorModel.model import VectorModel, WordVectorModel, TexTokenizer, WordTokenizer
 
 
 class Predictor:
     """Predictor for most similar documents."""
 
     def __init__(self, data_path):
-        self.eq_model = backend.models.VectorModel.model.VectorModel(data_path + 'nctr_se_latex2vec.model', data_path + 'q_and_a_t2v_features.npy',
-                                                                     backend.models.VectorModel.model.TexTokenizer())
-        self.text_model = backend.models.VectorModel.model.WordVectorModel(data_path + 'se_w2v.model', data_path + 'q_and_a_w2v_features.npy',
-                                                                           backend.models.VectorModel.model.WordTokenizer(data_path))
+        self.eq_model = VectorModel(data_path + 'nctr_se_latex2vec.model', data_path + 'q_and_a_t2v_features.npy',
+                                    TexTokenizer())
+        self.text_model = WordVectorModel(data_path + 'se_w2v.model', data_path + 'q_and_a_w2v_features.npy',
+                                          WordTokenizer(data_path))
 
-    def predict(self, text, code, equations, N=5):
+    def predict(self, text, code, equations, n=5):
         """Predicts the top N most similar document ids given text, code and equations (str)."""
         if text:
-            return self.text_model.predict(text, N)
+            return self.text_model.predict(text, n)
         elif equations:
-            return self.eq_model.predict(equations, N)
+            return self.eq_model.predict(equations, n)
         else:
-            return list(range(N))
+            return list(range(n))
 
-    def predict_by_id(self, id, N=5):
-        return self.eq_model.predict_by_id(id, N)
+    def predict_by_id(self, id, n=5):
+        return self.eq_model.predict_by_id(id, n)
