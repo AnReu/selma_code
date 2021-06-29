fcd1cd9ea50f0ff92a0cd5f94ffcf1486c9bde61
Guilherme Calabria Lopes <guicalabria@gmail.com>
2021-06-29 14:37:08 +0200

    integrate Flask_SQLAlchemy to our app

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,12 +1,21 @@
 import os
-from pathlib import Path
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+from pathlib import Path
 from backend.db_connection import DB
 from backend.search import search
 
 
 app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
+app.config.from_object(Config)
 
 PROJECT_DIR = str(Path(__file__).parents[1]) + '/'
-data_path = PROJECT_DIR + os.environ.get('DATA_DIR')
+DATA_PATH = PROJECT_DIR + os.environ.get('DATA_DIR')
 db = DB(PROJECT_DIR + os.environ.get('DB_PATH'))
+
+app_db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# from backend import api, models
