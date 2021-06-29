64ffc1050f83619700b075a62a6b2ac5b6f654be
Guilherme Calabria Lopes <guicalabria@gmail.com>
2021-06-29 12:50:28 +0200

    add seperated config file for flask app for sake of separation of concerns

---
+++ backend/config.py
@@ -0,0 +1,13 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    # Flask and some of its extensions use the value of the secret key
+    # as a cryptographic key, useful to generate signatures or tokens.
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '4r6ZmzajqSoSFn'
+
+    # Flask-SQLAlchemy configuration parameters
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
