fd3f29095ecfd38cb9671b9496309d6e95e12954
Guilherme Calabria Lopes <guicalabria@gmail.com>
2021-06-30 13:09:42 +0200

    style code according to PEP rules

--- backend/models/PyterrierModel/predictor.py
+++ backend/models/PyterrierModel/predictor.py
@@ -1,26 +1,28 @@
 import backend.models.PyterrierModel.textmodel
 
+
 class Predictor:
     """Predictor for most similar documents."""
 
     def __init__(self, data_path):
-        print("initialisation of the PyterrierMpdel.predictor")
-        #path to the data.properties of the used index
-        data_path = "/home/wilhelm/Uni/retrievalsystem/retrievalsystem_model/retrievalsystem/PyterrierModel/wt2g_index/data.properties"
+        print("initialisation of the PyTerrierModel.predictor")
+        # path to the data.properties of the used index
+        data_path = "/Users/gui/Development/repos/retrievalsystem/backend/models/PyterrierModel/wt2g_index/data" \
+                    ".properties "
         self.text_model = backend.models.PyterrierModel.textmodel.TF_IDFModel(data_path)
 
-    def predict(self, text, code, equations, N=5):
+    def predict(self, text, code, equations, n=5):
         """Predicts the top N most similar document ids given text, code and equations (str)."""
         print("predictor is used and this is the query:")
         print(text)
         if text:
             print("This is the return of the predictor:")
-            print(self.text_model.predict(text, N))
-            return self.text_model.predict(text, N)
+            print(self.text_model.predict(text, n))
+            return self.text_model.predict(text, n)
         elif equations:
-            return self.eq_model.predict(equations, N)
+            return self.eq_model.predict(equations, n)
         else:
-            return list(range(N))
+            return list(range(n))
 
     '''def predict_by_id(self, id, N=5):
         return self.eq_model.predict_by_id(id, N)'''
