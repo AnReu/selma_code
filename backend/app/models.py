7c7f9d6a79001f667a099853f9479e53c0e4ecf6
Guilherme Calabria Lopes <guicalabria@gmail.com>
2021-08-04 11:13:54 +0200

    add more attributes to QueryTemplate model

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,9 +3,14 @@ from backend.app import app_db as db
 
 class QueryTemplate(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    model_name = db.Column(db.String(80), unique=True, nullable=False)
-    model_language = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    model_name = db.Column(db.String(80), nullable=False)
+    model_language = db.Column(db.String(120), nullable=False)
+    query_text = db.Column(db.String(120), nullable=False)
+    user = db.Column(db.String(120), nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.name
+        return '<QueryTemplate %r>' % self.name
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
