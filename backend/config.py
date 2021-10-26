1d40d36f4c3a7db31285e7deab738d3eae4eb491
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2021-10-26 13:49:41 +0200

    move config.py to /backend

--- backend/config.py
+++ backend/config.py
@@ -10,20 +10,20 @@ class Config(object):
 
     # Flask-SQLAlchemy configuration parameters
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, '../app.db')
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     # SQLALCHEMY_TRACK_MODIFICATIONS = False disables, sending a signal to
     # the application every time a change is about to be made in the database.
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    DATA_DIR = ''
-    DB_PATH = ''
-    PYTERRIER_MODEL_PATH = ''
-    DB_TABLE_NAME = ''
-    DB_CONTENT_ATTRIBUTE_NAME = ''
+    __DATA_DIR = None
+    __DB_PATH = None
+    __PYTERRIER_MODEL_PATH = None
+    __DB_TABLE_NAME = None
+    __DB_CONTENT_ATTRIBUTE_NAME = None
 
     @staticmethod
     def load_rsenv() -> bool:
-        path = dotenv.find_dotenv(".rsenv")
+        path = dotenv.find_dotenv("app/.rsenv")
         rsconfig = dotenv.load_dotenv(path, encoding="utf-8")
         return rsconfig
 
@@ -33,3 +33,17 @@ class Config(object):
             Config.DATA_DIR = os.path.join(app_path, 'data')
         except Exception as error:
             print(f'oops... error = {error}')
+
+    @staticmethod
+    def get_db_path(self):
+        if self.__DB_PATH:
+            return self.__DB_PATH
+        try:
+            return os.environ.get('DB_PATH')
+        except Exception as error:
+            print(f'Could not find DB_PATH. Variable was not set in frondend nor using .rsenv')
+            raise
+
+
+
+
