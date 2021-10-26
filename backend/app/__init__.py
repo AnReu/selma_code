49291facdc116528ec0630f9f3c18e9d78700cc5
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2021-10-26 13:48:55 +0200

    add create_app

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,25 +1,41 @@
-import os
-
+# import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from backend.config import Config
 from flask_marshmallow import Marshmallow
-from .config import Config
-from .db_connection import DB
+# from .db_connection import DB
 
-# Load environment variables specific to the retrievalsystem app.
-Config.load_rsenv()
+db = SQLAlchemy()
+migrate = Migrate()
+ma = Marshmallow()
 
-app = Flask(__name__, static_folder='../../frontend/build', static_url_path='/')
-Config.set_data_dir(os.path.dirname(app.instance_path))
 
-app.config.from_object(Config)
-# app_db is the DB used by the frontend app. It stores things like QueryTemplates.
-app_db = SQLAlchemy(app)
-migrate = Migrate(app, app_db)
-ma = Marshmallow(app)
+def create_app(config_class=Config):
+    # # Load environment variables specific to the retrievalsystem app.
+    # Config.load_rsenv()
 
-# The var db is the DB used by information retrieval models, such as tf-idf, boolean and so on.
-db = DB(os.environ.get('DB_PATH'))
+    app = Flask(__name__)
+    # app = Flask(__name__, static_folder='../../frontend/build', static_url_path='/')
+    app.config.from_object(config_class)
 
-from backend.app import api, models # noqa
+    db.init_app(app)
+    migrate.init_app(app, db)
+    ma.init_app(app)
+
+
+    from backend.app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from backend.app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    # # The var db is the DB used by information retrieval models, such as tf-idf, boolean and so on.
+    # db = DB(os.environ.get('DB_PATH'))
+    # Config.set_data_dir(os.path.dirname(app.instance_path))
+
+    return app
+
+
+from backend.app import models
+# from backend.app import api, models # noqa
