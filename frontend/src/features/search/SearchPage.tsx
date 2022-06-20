a163a01d406be42751876e395141bece98c1e4a4
Guilherme Calabria Lopes <guicalabria@gmail.com>
2022-06-20 17:49:10 +0200

    Rename file NavBarWithTabs to NavBar

--- frontend/src/features/search/SearchPage.tsx
+++ frontend/src/features/search/SearchPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Stack from '@mui/material/Stack';
-import NavBar from '../navbar/NavbarWithTabs';
+import NavBar from '../navbar/NavBar';
 import SearchResults from './SearchResults';
 
 export default function SearchPage() {
