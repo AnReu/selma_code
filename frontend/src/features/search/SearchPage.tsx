4a3316c851b6a7c679ecec046af6e2f5d90e0364
Guilherme Calabria Lopes <guicalabria@gmail.com>
2022-06-21 18:22:52 +0200

    Fetch data structure when SearchPage is mouted

--- frontend/src/features/search/SearchPage.tsx
+++ frontend/src/features/search/SearchPage.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Stack from '@mui/material/Stack';
+import { useRecoilValue } from 'recoil';
 import NavBar from '../navbar/NavBar';
 import SearchResults from './SearchResults';
+import { dataStructureQueryState } from '../../recoil/selectors';
 
 export default function SearchPage() {
+  // Fetch data structure
+  useRecoilValue(dataStructureQueryState);
+
   return (
     <Stack id="search-page-stack">
       <NavBar />
