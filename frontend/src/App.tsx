20465c5dcd6c5d3fe003442fc088c3f3fcd940fb
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2022-06-22 19:15:28 +0200

    remove unused code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Grid from '@mui/material/Grid';
 import { MathJaxContext } from 'better-react-mathjax';
 import { RecoilRoot } from 'recoil';
 import RecoilizeDebugger from 'recoilize';
 import SearchPage from './features/search/SearchPage';
-import AboutPage from './features/AboutPage';
 import StoreSnackbar from './features/snackbar/StoreSnackbar';
 
 const config = {
@@ -28,20 +26,16 @@ export default function App() {
     <RecoilRoot>
       <RecoilizeDebugger />
       <React.Suspense fallback={<div>Loading...</div>}>
-        <BrowserRouter>
-          <Grid component="main" container sx={{ bgcolor: (theme) => theme.palette.background.paper, height: '100%' }}>
-            <Grid item xs={12}>
-              <MathJaxContext config={config}>
-                <Switch>
-                  <Route path="/about" component={AboutPage} />
-                  <Route path="/search" component={SearchPage} />
-                  <Route exact path="/" component={SearchPage} />
-                </Switch>
-                <StoreSnackbar />
-              </MathJaxContext>
-            </Grid>
+
+        <Grid component="main" container sx={{ bgcolor: (theme) => theme.palette.background.paper, height: '100%' }}>
+          <Grid item xs={12}>
+            <MathJaxContext config={config}>
+              <SearchPage />
+              <StoreSnackbar />
+            </MathJaxContext>
           </Grid>
-        </BrowserRouter>
+        </Grid>
+
       </React.Suspense>
     </RecoilRoot>
   );
