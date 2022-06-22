2758154c8a94cc161ea80ab5c70e7ed96b885916
Guilherme Calabria Lopes <guicalabria@gmail.com>
2022-06-22 18:36:00 +0200

    load configs when SearchPage is mounted, handle different states

--- frontend/src/features/search/SearchPage.tsx
+++ frontend/src/features/search/SearchPage.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
+import { useRecoilValueLoadable } from 'recoil';
 import Stack from '@mui/material/Stack';
-import { useRecoilValue } from 'recoil';
+import Container from '@mui/material/Container';
+import CircularProgress from '@mui/material/CircularProgress';
 import NavBar from '../navbar/NavBar';
 import SearchResults from './SearchResults';
-import { dataStructureQueryState } from '../../recoil/selectors';
+import {
+  dataStructureQueryState,
+  configsState,
+} from '../../recoil/selectors';
 
 export default function SearchPage() {
-  // Fetch data structure
-  useRecoilValue(dataStructureQueryState);
+  // Fetch data
+  const dataStructure = useRecoilValueLoadable(dataStructureQueryState);
+  const configs = useRecoilValueLoadable(configsState);
+
+  if (configs.state === 'hasError') {
+    return <h1>ERROR: Configs could not be fetched</h1>;
+  }
+
+  if (dataStructure.state === 'hasError') {
+    return <h1>ERROR: Data structure could not be fetched</h1>;
+  }
+
+  if (dataStructure.state === 'loading' || configs.state === 'loading') {
+    return (
+      <Container
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+        }}
+      >
+        <CircularProgress />
+      </Container>
+    );
+  }
 
   return (
     <Stack id="search-page-stack">
