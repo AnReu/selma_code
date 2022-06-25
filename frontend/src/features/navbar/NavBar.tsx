d7a9c007ca421bcbf3ccd4a11d35638a71655123
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2022-06-25 12:21:17 +0200

    add new version of navbar

--- frontend/src/features/navbar/NavBar.tsx
+++ frontend/src/features/navbar/NavBar.tsx
@@ -1,94 +1,45 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
-import { useTheme } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import SettingsIcon from '@mui/icons-material/Settings';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import { useRecoilValue } from 'recoil';
 import TemplateListDialog from '../templates/TemplateListDialog';
-import DefaultSearchBar from '../search/DefaultSearchBar';
-import SeparatedSearchBar from '../search/SeparatedSearchBar';
-import URLSearchBar from '../search/URLSearchBar';
-import FileUploadSearchBar from '../search/FileUploadSearchBar';
-import SettingsDialog from './SettingsDialog';
-import { selectMode, setMode, SearchMode } from '../search/searchSlice';
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
 import { configsState } from '../../recoil/selectors';
+import SettingsDialog from './SettingsDialog';
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  dir?: string;
-  currentMode: string;
-  value: string;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const {
-    children, value, currentMode, ...other
-  } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== currentMode}
-      id={`tabpanel-${currentMode}`}
-      aria-labelledby={`tab-${currentMode}`}
-      {...other}
-    >
-      {value === currentMode && (
-        <Box sx={{
-          display: 'flex',
-          minHeight: (theme) => theme.spacing(50),
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-        >
-          {children}
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `tab-${index}`,
-    'aria-controls': `tabpanel-${index}`,
-  };
-}
+const navItems = ['Default', 'Separated', 'ID or URL', 'File'];
 
 export default function NavBar() {
-  const dispatch = useAppDispatch();
-  const theme = useTheme();
   const configs = useRecoilValue(configsState);
-  const mode = useAppSelector(selectMode);
   const [isDialogOpen, setIsDialogOpen] = React.useState<boolean>(false);
 
-  const handleCloseDialog = () => {
-    setIsDialogOpen(false);
-  };
-
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    dispatch(setMode(newValue as SearchMode));
-  };
-
   return (
-    <Box sx={{ bgcolor: 'background.paper' }}>
+    <Box sx={{ bgcolor: 'pink' }}>
       <AppBar position="static">
         <Toolbar>
-
           <Typography
             variant="h6"
             noWrap
             component="div"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
           >
             RetrievalSystem
           </Typography>
+
+          <Box sx={{ flexGrow: 1 }} />
+
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff', mx: 2 }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
 
           <Box sx={{ flexGrow: 1 }} />
 
@@ -105,33 +56,8 @@ export default function NavBar() {
           </IconButton>
 
         </Toolbar>
-        <Tabs
-          value={mode}
-          onChange={handleChange}
-          indicatorColor="secondary"
-          textColor="inherit"
-          variant="fullWidth"
-        >
-          <Tab disabled={!configs.allowed_search_modes.default} value="default" label="Default" {...a11yProps(0)} />
-          <Tab disabled={!configs.allowed_search_modes.separated} value="separated" label="Separated" {...a11yProps(1)} />
-          <Tab disabled={!configs.allowed_search_modes.url} value="url" label="ID or URL" {...a11yProps(2)} />
-          <Tab disabled={!configs.allowed_search_modes.file} value="file" label="File" {...a11yProps(2)} />
-        </Tabs>
       </AppBar>
-
-      <TabPanel value="default" currentMode={mode} dir={theme.direction}>
-        <DefaultSearchBar />
-      </TabPanel>
-      <TabPanel value="separated" currentMode={mode} dir={theme.direction}>
-        <SeparatedSearchBar />
-      </TabPanel>
-      <TabPanel value="url" currentMode={mode} dir={theme.direction}>
-        <URLSearchBar />
-      </TabPanel>
-      <TabPanel value="file" currentMode={mode} dir={theme.direction}>
-        <FileUploadSearchBar />
-      </TabPanel>
-      <SettingsDialog isOpen={isDialogOpen} onClose={handleCloseDialog} />
+      <SettingsDialog isOpen={isDialogOpen} onClose={() => setIsDialogOpen(false)} />
     </Box>
   );
 }
---
+++ frontend/src/features/navbar/NavBarOld.tsx
@@ -0,0 +1,137 @@
+import React from 'react';
+import { useTheme } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { useRecoilValue } from 'recoil';
+import TemplateListDialog from '../templates/TemplateListDialog';
+import DefaultSearchBar from '../search/DefaultSearchBar';
+import SeparatedSearchBar from '../search/SeparatedSearchBar';
+import URLSearchBar from '../search/URLSearchBar';
+import FileUploadSearchBar from '../search/FileUploadSearchBar';
+import SettingsDialog from './SettingsDialog';
+import { selectMode, setMode, SearchMode } from '../search/searchSlice';
+import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { configsState } from '../../recoil/selectors';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  currentMode: string;
+  value: string;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const {
+    children, value, currentMode, ...other
+  } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== currentMode}
+      id={`tabpanel-${currentMode}`}
+      aria-labelledby={`tab-${currentMode}`}
+      {...other}
+    >
+      {value === currentMode && (
+        <Box sx={{
+          display: 'flex',
+          minHeight: (theme) => theme.spacing(50),
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        >
+          {children}
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `tab-${index}`,
+    'aria-controls': `tabpanel-${index}`,
+  };
+}
+
+export default function NavBarOld() {
+  const dispatch = useAppDispatch();
+  const theme = useTheme();
+  const configs = useRecoilValue(configsState);
+  const mode = useAppSelector(selectMode);
+  const [isDialogOpen, setIsDialogOpen] = React.useState<boolean>(false);
+
+  const handleCloseDialog = () => {
+    setIsDialogOpen(false);
+  };
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    dispatch(setMode(newValue as SearchMode));
+  };
+
+  return (
+    <Box sx={{ bgcolor: 'background.paper' }}>
+      <AppBar position="static">
+        <Toolbar>
+
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' } }}
+          >
+            RetrievalSystem
+          </Typography>
+
+          <Box sx={{ flexGrow: 1 }} />
+
+          <TemplateListDialog />
+
+          <IconButton
+            size="large"
+            edge="end"
+            aria-haspopup="true"
+            color="inherit"
+            onClick={() => setIsDialogOpen(true)}
+          >
+            <SettingsIcon />
+          </IconButton>
+
+        </Toolbar>
+        <Tabs
+          value={mode}
+          onChange={handleChange}
+          indicatorColor="secondary"
+          textColor="inherit"
+          variant="fullWidth"
+        >
+          <Tab disabled={!configs.allowed_search_modes.default} value="default" label="Default" {...a11yProps(0)} />
+          <Tab disabled={!configs.allowed_search_modes.separated} value="separated" label="Separated" {...a11yProps(1)} />
+          <Tab disabled={!configs.allowed_search_modes.url} value="url" label="ID or URL" {...a11yProps(2)} />
+          <Tab disabled={!configs.allowed_search_modes.file} value="file" label="File" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+
+      <TabPanel value="default" currentMode={mode} dir={theme.direction}>
+        <DefaultSearchBar />
+      </TabPanel>
+      <TabPanel value="separated" currentMode={mode} dir={theme.direction}>
+        <SeparatedSearchBar />
+      </TabPanel>
+      <TabPanel value="url" currentMode={mode} dir={theme.direction}>
+        <URLSearchBar />
+      </TabPanel>
+      <TabPanel value="file" currentMode={mode} dir={theme.direction}>
+        <FileUploadSearchBar />
+      </TabPanel>
+      <SettingsDialog isOpen={isDialogOpen} onClose={handleCloseDialog} />
+    </Box>
+  );
+}
