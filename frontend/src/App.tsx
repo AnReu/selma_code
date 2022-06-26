815e50feed6d66e7c0d3b451e1c30b0040a49c18
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2022-06-26 17:08:14 +0200

    add custom theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { MathJaxContext } from 'better-react-mathjax';
 import { RecoilRoot } from 'recoil';
 import RecoilizeDebugger from 'recoilize';
 import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import SearchPage from './features/search/SearchPage';
 import StoreSnackbar from './features/snackbar/StoreSnackbar';
 import ResultsPage from './ResultsPage';
@@ -23,19 +24,49 @@ const config = {
   },
 };
 
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#d79921',
+    },
+    secondary: {
+      main: '#458588',
+    },
+    background: {
+      default: '#282828',
+      paper: '#3c3836',
+    },
+    error: {
+      main: '#fb4934',
+    },
+    warning: {
+      main: '#fabd2f',
+    },
+    info: {
+      main: '#83a598',
+    },
+    success: {
+      main: '#b8bb26',
+    },
+  },
+});
+
 export default function App() {
   return (
     <RecoilRoot>
       <RecoilizeDebugger />
       <React.Suspense fallback={<div>Loading...</div>}>
-        <MathJaxContext config={config}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="search" element={<SearchPage />} />
-            <Route path="results" element={<ResultsPage />} />
-          </Routes>
-          <StoreSnackbar />
-        </MathJaxContext>
+        <ThemeProvider theme={theme}>
+          <MathJaxContext config={config}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="search" element={<SearchPage />} />
+              <Route path="results" element={<ResultsPage />} />
+            </Routes>
+            <StoreSnackbar />
+          </MathJaxContext>
+        </ThemeProvider>
       </React.Suspense>
     </RecoilRoot>
   );
