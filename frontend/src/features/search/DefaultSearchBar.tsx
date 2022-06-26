654d83659ec59f10918c64bdb3c4a5795a204e91
Guilherme Calabria Lopes <guicalabria@gmail.com>
2022-06-26 19:23:40 +0200

    Remove tooltips

--- frontend/src/features/search/DefaultSearchBar.tsx
+++ frontend/src/features/search/DefaultSearchBar.tsx
@@ -7,7 +7,6 @@ import IconButton from '@mui/material/IconButton';
 import InputBase from '@mui/material/InputBase';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
-import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
 // MUI Icons
 import AddCircleIcon from '@mui/icons-material/AddCircle';
@@ -23,7 +22,6 @@ export default function DefaultSearchBar() {
   const inputDefaultSearchEl = React.useRef<HTMLInputElement>(null);
   // State
   const [open, setOpen] = React.useState(false);
-  const [showTooltip, setShowTooltip] = React.useState(true);
   const [query, setQuery] = useRecoilState(queryParametersState);
 
   // Handlers
@@ -42,7 +40,8 @@ export default function DefaultSearchBar() {
     }
   };
 
-  const onKeyCombo = () => {
+  const onKeyCombo = (shortcut: string, event: KeyboardEvent) => {
+    event.preventDefault();
     inputDefaultSearchEl.current?.focus();
   };
 
@@ -54,53 +53,45 @@ export default function DefaultSearchBar() {
       >
 
         <Stack>
-          <Tooltip
-            open={showTooltip}
-            placement="top-start"
-            title="Try ⌘+K"
+          <Paper
+            component="form"
+            sx={{
+              p: '2px 4px',
+              display: 'flex',
+              alignItems: 'center',
+              width: 800,
+              borderRadius: '30px',
+              m: 'auto',
+            }}
           >
-            <Paper
-              component="form"
-              sx={{
-                p: '2px 4px',
-                display: 'flex',
-                alignItems: 'center',
-                width: 800,
-                borderRadius: '30px',
-                m: 'auto',
-              }}
+
+            <InputBase
+              inputRef={inputDefaultSearchEl}
+              sx={{ ml: 2, flex: 1 }}
+              placeholder="Search"
+              inputProps={{ 'aria-label': 'search' }}
+              value={query.text}
+              onChange={handleQueryTextChange}
+              onKeyDown={handleEnter}
+            />
+
+            <IconButton sx={{ p: '10px' }} aria-label="open advanced search" onClick={() => setOpen(true)}>
+              <AddCircleIcon />
+            </IconButton>
+
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+
+            <Link to={{
+              pathname: 'results',
+              search: '?text=image&db=codeSearchNet_java&model=PyterrierModel&index=comment',
+            }}
             >
-              <InputBase
-                inputRef={inputDefaultSearchEl}
-                sx={{ ml: 2, flex: 1 }}
-                placeholder="Search"
-                inputProps={{ 'aria-label': 'search' }}
-                value={query.text}
-                onChange={handleQueryTextChange}
-                onKeyDown={handleEnter}
-                onFocus={() => setShowTooltip(false)}
-              />
-              <Tooltip title="Create query template">
-                <IconButton sx={{ p: '10px' }} aria-label="open advanced search" onClick={() => setOpen(true)}>
-                  <AddCircleIcon />
-                </IconButton>
-              </Tooltip>
+              <IconButton sx={{ p: '10px' }} onClick={handleSearch} aria-label="run search">
+                <SearchIcon />
+              </IconButton>
+            </Link>
 
-              <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-
-              <Tooltip title="Run search">
-                <Link to={{
-                  pathname: 'results',
-                  search: '?text=image&db=codeSearchNet_java&model=PyterrierModel&index=comment',
-                }}
-                >
-                  <IconButton sx={{ p: '10px' }} onClick={handleSearch} aria-label="run search">
-                    <SearchIcon />
-                  </IconButton>
-                </Link>
-              </Tooltip>
-            </Paper>
-          </Tooltip>
+          </Paper>
 
           <Box sx={{ width: 800, textAlign: 'center' }}>
             <Typography color="textPrimary" variant="caption">
