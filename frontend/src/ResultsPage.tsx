dba9d9d2e97227118609c4312ae6990dd03a67df
Guilherme Calabria Lopes <guicalabria@gmail.com>
2022-06-29 12:48:31 +0200

    set queryState from URL

--- frontend/src/ResultsPage.tsx
+++ frontend/src/ResultsPage.tsx
@@ -5,8 +5,11 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Container from '@mui/material/Container';
 import Pagination from '@mui/material/Pagination/Pagination';
 import PaginationItem from '@mui/material/PaginationItem/PaginationItem';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import SearchResult from './features/search/SearchResult';
 import ResultsNavbar from './features/navbar/ResultsNavbar';
+import { queryStringState } from './recoil/selectors';
+import { queryState } from './recoil/atoms';
 
 export interface Result {
   acceptedAnswerId?: number | null;
@@ -25,16 +28,25 @@ export interface Result {
 
 export default function ResultsPage() {
   const [searchParams] = useSearchParams();
-  const text = searchParams.get('text');
-  const db = searchParams.get('db');
-  const model = searchParams.get('model');
-  const index = searchParams.get('index');
-
   const [results, setResults] = React.useState<Result[] | undefined>(undefined);
   const [isLoading, setIsLoading] = React.useState(true);
   const [endIndex] = React.useState(10);
+  const queryString = useRecoilValue(queryStringState);
+  const [query, setQuery] = useRecoilState(queryState);
+  const page = Number(searchParams.get('page')) as number;
+  const text = searchParams.get('text') as string;
+  const db = searchParams.get('db') as string;
+  const model = searchParams.get('model') as string;
+  const index = searchParams.get('index') as string;
 
   React.useEffect(() => {
+    setQuery({
+      ...query,
+      ...{
+        text, db, model, index, page,
+      },
+    });
+
     const getResults = async () => {
       const URL = 'http://127.0.0.1:5000/api/v1/search?'
         + `text=${text}&`
@@ -42,8 +54,9 @@ export default function ResultsPage() {
         + `model=${model}&`
         + `index=${index}&`
         + `page=${page}&`
+        + 'model-language=english';
 
-      const response = await fetch(URL);
+      const response = await fetch(URL, { mode: 'cors' });
       const data = await response.json();
 
       setResults(data.results);
