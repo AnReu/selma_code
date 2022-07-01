966f901cbcbe61e7dc3decdb1cc4072a5e8a63c6
Guilherme Calabria Lopes <guicalabria@gmail.com>
2022-07-01 14:18:59 +0200

    add ExamplesDialog

---
+++ frontend/src/features/examples/ExamplesDialog.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import ExamplesList from './ExamplesList';
+
+export default function ExamplesDialog() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Button sx={{ mr: 4 }} variant="outlined" onClick={handleClickOpen}>
+        Examples
+      </Button>
+      <Dialog
+        fullWidth
+        maxWidth="sm"
+        open={open}
+        onClose={handleClose}
+      >
+        <DialogTitle>Available Query Templates</DialogTitle>
+        <DialogContent>
+          <ExamplesList />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
