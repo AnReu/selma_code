ff6fff701f2bc78221e7ac09e343416293ad94f7
Guilherme Calabria Lopes <guicalabria@gmail.com>
2022-07-04 10:22:16 +0200

    change logo color according to current theme

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 // import DefaultSearchBar from './features/search/DefaultSearchBar';
-import DDBGLogo from './assets/dresden_db_group_logo.svg';
+import { useTheme } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import DDBGLogo from './assets/SELMA-Logo_code.svg';
 import SearchForm from './features/SearchForm';
 
 export default function HomePage() {
+  const theme = useTheme();
+
+  const logoClass = theme.palette.mode === 'light' ? 'dark-logo' : 'light-logo';
+
   return (
     <Box sx={{
       textAlign: 'center',
       minWidth: '80%',
     }}
     >
-      <img style={{ maxWidth: '160px', margin: '32px' }} src={DDBGLogo} alt="Dresden DB Group" />
+      <img
+        className={logoClass}
+        src={DDBGLogo}
+        alt="Dresden DB Group"
+      />
+      <Typography
+        variant="h3"
+        sx={{
+          color: 'text.primary',
+          fontWeight: 800,
+          mb: 2,
+        }}
+      >
+        SELMA
+      </Typography>
       {/* <DefaultSearchBar /> */}
       <SearchForm />
     </Box>
